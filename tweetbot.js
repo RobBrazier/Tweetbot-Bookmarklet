ebfb59880e042bd0adfec24e3bca585233cee659
Rob Brazier <rob.brazier@me.com>
2013-07-01 13:20:23 +0100

    updated to remove "Uncaught Error"

--- tweetbot.js
+++ tweetbot.js
@@ -3,35 +3,35 @@ if(window.location.hostname.indexOf('twitter.com') !== -1){
 
   if(document.getElementById('dm_dialog').style.display === "block"){
     window.location = 'tweetbot://direct_messages';
-    throw 'exit';
+    return false;
   }
 
   // Check if is homepage
 
   if(window.location.pathname == "/"){
     window.location = 'tweetbot:///timeline';
-    throw 'exit';
+    return false;
   }
 
   // Check if viewing lists
 
   if(window.location.pathname.split('/')[2] == "lists"){
     window.location = 'tweetbot:///lists';
-    throw 'exit';
+    return false;
   }
 
   // Check if viewing favourites
 
   if(window.location.pathname.split('/')[1] == "favorites"){
     window.location = 'tweetbot:///favorites';
-    throw 'exit';
+    return false;
   }
 
   // Check if viewing status
 
   if(window.location.pathname.split('/')[2] == "status"){
     window.location = 'tweetbot:///status/'+window.location.pathname.split('/')[3];
-    throw 'exit';
+    return false;
   }
 
   // Check if searching
@@ -40,16 +40,16 @@ if(window.location.hostname.indexOf('twitter.com') !== -1){
     var search = window.location.search.split('&')[0];
     search = search.split('?q=')[1];
     window.location = 'tweetbot:///search?query='+search;
-    throw 'exit';
+    return false;
   } else if(window.location.pathname.split('/')[1] == "search-home"){
     window.location = 'tweetbot:///search';
-    throw 'exit';
+    return false;
   }
 
   // Check if viewing user profile
 
   if(window.location.pathname.split('/')[1] && !window.location.pathname.split('/')[2] && window.location.pathname.split('/')[1] !== 'lists' && window.location.pathname.split('/')[1] !== 'favourites' && window.location.pathname.split('/')[1] !== 'status' && window.location.pathname.split('/')[1] !== 'search' && window.location.pathname.split('/')[1] !== 'search-home'){
     window.location = 'tweetbot:///user_profile/'+window.location.pathname.split('/')[1];
-    throw 'exit';
+    return false;
   }
 }
