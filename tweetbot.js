d5ce779930f3be68123afa7783eab0ab8bf627ec
Rob Brazier <rob.brazier@me.com>
2014-06-21 15:10:26 +0100

    Changed from rawgithub.com to rawgit.com, saves a redirect and removed unnecessary code duplication

--- tweetbot.js
+++ tweetbot.js
@@ -2,42 +2,37 @@ if (window.location.hostname.indexOf("twitter.com") !== -1) {
   var pathname = window.location.pathname.split("/");
 
   // Check if is homepage
-
-  if (window.location.pathname == "/") {
+  if (window.location.pathname === "/") {
     window.location = "tweetbot:///timeline";
   }
 
   // Check if viewing lists
-
-  if (pathname[2] == "lists") {
+  if (pathname[2] === "lists") {
     window.location = "tweetbot:///lists";
   }
 
   // Check if viewing favourites
-
-  if (pathname[1] == "favorites") {
+  if (pathname[1] === "favorites") {
     window.location = "tweetbot:///favorites";
   }
 
   // Check if viewing status
-
-  if (pathname[2] == "status") {
+  if (pathname[2] === "status") {
     window.location = "tweetbot:///status/" + pathname[3];
   }
 
   // Check if searching
-
-  if (pathname[1] == "search" && window.location.search) {
+  if (pathname[1] === "search" && window.location.search) {
     var search = window.location.search.split("&")[0];
     search = search.split("?q=")[1];
     window.location = "tweetbot:///search?query=" + search;
-  } else if (pathname[1] == "search-home") {
+  } else if (pathname[1] === "search-home") {
     window.location = "tweetbot:///search";
   }
 
   // Check if viewing user profile
-
-  if (pathname.length == 2 && pathname[1] !== "lists" && pathname[1] !== "favourites" && pathname[1] !== "status" && pathname[1] !== "search" && pathname[1] !== "search-home") {
+  var user_profile_excludes = ["lists", "favourites", "status", "search", "search-home"];
+  if (pathname.length === 2 && user_profile_excludes.indexOf(pathname[1]) === -1) {
     window.location = "tweetbot:///user_profile/" + pathname[1];
   }
 }
